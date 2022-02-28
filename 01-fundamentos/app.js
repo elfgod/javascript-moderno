5fe2f5b8efb4391568f2ffce41c0589cbc1d40b6
ElfGod <albertogx@icloud.com>
2022-02-27 22:42:33 -0500

    Introduccion a variables y comentarios

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,28 @@
+console.log('Hola Mundo')
+
+console.log('Comentarios')
+// comentario de 1 linea
+/* 
+comentarios de
+multilineas
+*/
+
+console.log('Intro a variables')
+let a = 10;
+var b = 10;
+const c = 10;
+// c = 20; // esto no se puede hacer por que es una variable constante
+a = 25; 
+b = 35;
+
+console.log(a);
+console.log(b);
+console.log(a+b);
+
+// declarar varias variables en 1 sola linea
+// let x, y, z;
+// let o = 1, p = 2, q = 3;
+
+// let x = 1,
+//		 y = 2,
+//     z = 3
---
+++ README.md
@@ -0,0 +1,9 @@
+# JavaScript Moderno
+
+## 01-Fundamentos
+
+### Introduccion a variables y comentario
+
+1. COMENTARIOS // COMENTAR
+Son lineas de codigo que el interprete de JavaScript
+ignorara a la hora de ejecutarlo
