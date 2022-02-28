85a1a2c5f307da3ad9a49068371c8c331e907111
ElfGod <albertogx@icloud.com>
2022-02-27 23:15:01 -0500

    Introduccion a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,13 +1,15 @@
 console.log('Hola Mundo')
 
-console.log('Comentarios')
+console.log('\n');
+console.log('%c Comentarios', 'color: orange; font-weight: bold; font-size: 16px');
 // comentario de 1 linea
 /* 
 comentarios de
 multilineas
 */
 
-console.log('Intro a variables')
+console.log('\n');
+console.log('%c Intro a variables', 'color: orange; font-weight: bold; font-size: 16px');
 let a = 10;
 var b = 10;
 const c = 10;
@@ -26,3 +28,38 @@ console.log(a+b);
 // let x = 1,
 //		 y = 2,
 //     z = 3
+
+console.log('\n');
+console.log('%c Intro a la consola', 'color: orange; font-weight: bold; font-size: 16px');
+console.log('a: ', a);
+console.warn('b: ', b);
+console.error('a+b: ', a+b);
+console.info('a: ', a);
+console.table('b: ', b)
+
+console.log('\n');
+console.log({a});
+console.warn({b});
+console.error('a + b: ', a+b);
+console.info({a});
+console.table({b})
+
+console.log('\n');
+let string1 = 'hola';
+let string2 = 'Spiderman'
+console.log(string1 + ' ' + string2);
+console.table({string1, string2})
+
+const saludo = string1 + string2
+console.log('saludo: ', saludo)
+
+
+
+
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
